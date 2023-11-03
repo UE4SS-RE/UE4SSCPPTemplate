c567d7d1131e4953226016b2d7da7b77d0fd019b
narknon <73571427+narknon@users.noreply.github.com>
2023-11-03 13:29:19 -0400

    Update build_mods_shipping.bat

--- build_mods_shipping.bat
+++ build_mods_shipping.bat
@@ -1,5 +1,5 @@
 if not exist "BuildFiles" mkdir BuildFiles
 cd BuildFiles
 cmake -B Output -G"Visual Studio 17 2022" ..
-cmake --build . --config Game__Debug__Win64
+cmake --build . --config Game__Shipping__Win64
 pause
