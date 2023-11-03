bb8716f84d46d0fb0e66de9388dfeeba8ee4b34a
narknon <73571427+narknon@users.noreply.github.com>
2023-11-03 12:57:43 -0400

    Initial commit

---
+++ MyCPPMods/MyExampleMod/src/dllmain.cpp
@@ -0,0 +1,66 @@
+﻿#include <DynamicOutput/Output.hpp>
+#include <Mod/CppUserModBase.hpp>
+#include <Unreal/UObjectGlobals.hpp>
+#include <Unreal/UObject.hpp>
+
+namespace MyMods
+{
+    using namespace RC;
+    using namespace Unreal;
+    
+    /**
+    * MyExampleMod: UE4SS c++ mod class defintion
+    */
+    class MyExampleMod : public RC::CppUserModBase {
+    public:
+        
+        // constructor
+        MyExampleMod() {
+            ModVersion = STR("0.1");
+            ModName = STR("MyExampleMod");
+            ModAuthors = STR("UE4SS");
+            ModDescription = STR("A basic template C++ mod");
+            // Do not change this unless you want to target a UE4SS version
+            // other than the one you're currently building with somehow.
+            //ModIntendedSDKVersion = STR("2.6");
+            
+            Output::send<LogLevel::Warning>(STR("[MyExampleMod]: Init.\n"));
+        }
+        
+        // destructor
+        ~MyExampleMod() override {
+            // fill when required
+        }
+
+        auto on_program_start() -> void override
+        {
+        }
+
+        auto on_dll_load(std::wstring_view dll_name) -> void override
+        {
+        }
+
+        auto on_unreal_init() -> void override
+        {
+            // You are allowed to use the 'Unreal' namespace in this function and anywhere else after this function has fired.
+            auto Object = UObjectGlobals::StaticFindObject<UObject*>(nullptr, nullptr, STR("/Script/CoreUObject.Object"));
+            Output::send<LogLevel::Verbose>(STR("Object Name: {}\n"), Object->GetFullName());
+        }
+
+    };//class
+}
+
+/**
+* export the start_mod() and uninstall_mod() functions to
+* be used by the core ue4ss system to load in our dll mod
+*/
+#define MOD_EXPORT __declspec(dllexport) 
+extern "C" {
+    MOD_EXPORT RC::CppUserModBase* start_mod(){ return new MyMods::MyExampleMod(); }
+    MOD_EXPORT void uninstall_mod(RC::CppUserModBase* mod) { delete mod; }
+}
+    
+
+
+
+
---
+++ build_mods_shipping.bat
@@ -0,0 +1,5 @@
+if not exist "BuildFiles" mkdir BuildFiles
+cd BuildFiles
+cmake -B Output -G"Visual Studio 17 2022" ..
+cmake --build . --config Game__Debug__Win64
+pause
