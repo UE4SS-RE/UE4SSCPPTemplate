395f5980be1a58d56c299c0a68ff7f39489bc2c2
George Higashiyama <george@higashiyama.us>
2024-04-13 17:44:15 -0700

    Allow for submod update for first time run

--- Scripts/ue4ss_git.lua
+++ Scripts/ue4ss_git.lua
@@ -21,54 +21,57 @@ function main()
         git.clone(ue4ss_url)
     end
 
-    -- Special handling if the user wants to find the 'latest' release tag.
-    if remote == "latest" then
-        print("Attempting to find latest release tag...")
-        local tags = git.tags(ue4ss_url)
-        local latest_tag
-        for _,tag in ipairs(tags) do
-            if semver.is_valid(tag) then
-                if not latest_tag then
-                    latest_tag = tag
-                end
+    if remote then
+        -- Special handling if the user wants to find the 'latest' release tag.
+        if remote == "latest" then
+            print("Attempting to find latest release tag...")
+            local tags = git.tags(ue4ss_url)
+            local latest_tag
+            for _,tag in ipairs(tags) do
+                if semver.is_valid(tag) then
+                    if not latest_tag then
+                        latest_tag = tag
+                    end
 
-                if semver.compare(tag, latest_tag) == 1 then
-                    latest_tag = tag
+                    if semver.compare(tag, latest_tag) == 1 then
+                        latest_tag = tag
+                    end
                 end
             end
+            remote = latest_tag
         end
-        remote = latest_tag
-    end
 
-    -- Returns a matching version/tag/branch based on the provided "remote" string.
-    -- Uses semantic versioning or branch naming.
-    local remote, type = semver.select(remote, nil, git.tags(ue4ss_url), git.branches(ue4ss_url))
-    if not remote then
-        print("%s was not found as a valid git remote.")
-        os.exit()
-    end
-
-    if type == "tag" then
-        -- Special logic to ensure we don't check out a tag that doesn't have xmake.
-        -- This would put the user's local repo into an unrecoverable state.
-        if not semver.satisfies(remote, ">3.0.1") then
-            print("The latest tag %s is not compatible with xmake. Using 'main' branch instead of a tag.", remote)
-            remote = "main"
-            type = "branch"
+        -- Returns a matching version/tag/branch based on the provided "remote" string.
+        -- Uses semantic versioning or branch naming.
+        local remote, type = semver.select(remote, nil, git.tags(ue4ss_url), git.branches(ue4ss_url))
+        if not remote then
+            print("%s was not found as a valid git remote.")
+            os.exit()
         end
-    end
 
-    print("Checking out %s %s...", type, remote)
-    git.checkout(remote,{repodir=ue4ss_dir})
+        if type == "tag" then
+            -- Special logic to ensure we don't check out a tag that doesn't have xmake.
+            -- This would put the user's local repo into an unrecoverable state.
+            if not semver.satisfies(remote, ">3.0.1") then
+                print("The latest tag %s is not compatible with xmake. Using 'main' branch instead of a tag.", remote)
+                remote = "main"
+                type = "branch"
+            end
+        end
+
+        print("Checking out %s %s...", type, remote)
+        git.checkout(remote,{repodir=ue4ss_dir})
+    end
 
     if update then
         -- If the user specifies a tag, then we don't need to pull/update.
         local cur_branch = git.branch({repodir = ue4ss_dir})
         if cur_branch then
             git.pull({remote = "origin", tags = true, repodir = ue4ss_dir})
-            submodule.update({repodir = ue4ss_dir, recursive = true})
         else
-            print("UE4SS is not currently a branch. Update has no effect")
+            print("UE4SS is not currently a branch. No remote changes to pull.")
         end
+
+        submodule.update({repodir = ue4ss_dir, recursive = true})
     end
 end
